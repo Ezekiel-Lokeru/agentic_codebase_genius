2246e1dbc14993aaed590bde7d532801f4001c5f
Unknownymous-404-sudo <ezekiellokeru@gmail.com>
2025-11-14 14:44:38 +0300

    run jac

---
+++ run_jac.py
@@ -0,0 +1,6 @@
+# run_jac.py
+# Import the typing override patch first to ensure override is available
+import typing_override_patch  # noqa: F401
+
+import os
+os.system("jac run jac/repo_mapper.jac")
